79596072b52be49889a23e0f66ebf8f17f431800
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-24 19:39:23 +0530

    Fontawesome package added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import ReactMapGl from "react-map-gl";
+import ReactMapGl, { Marker } from "react-map-gl";
+import { Mapbox } from "./intefaces/mapbox-interface";
 import "./App.css";
 
 const App: React.FC = () => {
-  const [viewport, setViewPort] = useState({
+  const [viewport, setViewPort] = useState<Mapbox>({
     latitude: 45.4211,
     longitude: -75.6903,
     zoom: 10,
@@ -14,10 +15,10 @@ const App: React.FC = () => {
     <div className="mapbox_container">
       <ReactMapGl
         {...viewport}
+        mapStyle="mapbox://styles/sudosdm/cknvrogsv1vt217jgicvef63y"
+        onViewportChange={(viewport: Mapbox) => setViewPort(viewport)}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      >
-        markers here
-      </ReactMapGl>
+      ></ReactMapGl>
     </div>
   );
 };
---
+++ src/intefaces/mapbox-interface.ts
@@ -0,0 +1,7 @@
+export interface Mapbox {
+  latitude: 45.4211;
+  longitude: -75.6903;
+  zoom: 10;
+  width: "100%";
+  height: "100%";
+}
