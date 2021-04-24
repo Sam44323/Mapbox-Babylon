d9e176a7dd40825b6c209f748121731839349a0b
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-24 18:34:52 +0530

    Completed the basic setup for creating displaying a map

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGl from "react-map-gl";
 import "./App.css";
 
-function App() {
-  return <div className="App">mappppp</div>;
-}
+const App: React.FC = () => {
+  const [viewport, setViewPort] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    zoom: 10,
+    width: "100%",
+    height: "100%",
+  });
+  return (
+    <div className="mapbox_container">
+      <ReactMapGl
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      >
+        markers here
+      </ReactMapGl>
+    </div>
+  );
+};
 
 export default App;
