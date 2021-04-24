d97e699ca42b63a27481863d6003e82f4b64fa68
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-24 20:05:07 +0530

    MapRoot and MapDisplay component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
-import React, { useState } from "react";
-import ReactMapGl, { Marker } from "react-map-gl";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapPin } from "@fortawesome/free-solid-svg-icons";
-import { Mapbox } from "./intefaces/mapbox-interface";
-import "./App.css";
+import React from "react";
+import MapRoot from "./container/MapRoot";
 
 const App: React.FC = () => {
-  const [viewport, setViewPort] = useState<Mapbox>({
-    latitude: 45.4211,
-    longitude: -75.6903,
-    zoom: 10,
-    width: "100%",
-    height: "100%",
-  });
-  return (
-    <div className="mapbox_container">
-      <ReactMapGl
-        {...viewport}
-        mapStyle="mapbox://styles/sudosdm/cknvrogsv1vt217jgicvef63y"
-        onViewportChange={(viewport: Mapbox) => setViewPort(viewport)}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      >
-        <Marker latitude={45.4211} longitude={-75.6903}>
-          <FontAwesomeIcon icon={faMapPin} size="3x" color="white" />
-        </Marker>
-      </ReactMapGl>
-    </div>
-  );
+  return <MapRoot />;
 };
-
 export default App;
---
+++ src/container/MapRoot.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MapDisplay from "../components/MapDisplay";
+
+const MapRoot: React.FC = () => {
+  return (
+    <>
+      <MapDisplay />
+    </>
+  );
+};
+
+export default MapRoot;
